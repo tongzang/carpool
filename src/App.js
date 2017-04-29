a1babe590c8c20e4b964eed0baa6cfeb48c0ddf0
Jun <jun.srisu@gmail.com>
2017-04-29 13:49:12 +0700

    put code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+
+  injectTapEventPlugin();
+
 
 class App extends Component {
   render() {
+    const muiTheme = getMuiTheme({
+palette: {
+    primary1Color: '#E4BE55',
+    accent1Color: '#000000',
+    textColor: '#E4BE55',
+    alternateTextColor: '#FFFFFF'
+  },
+      appBar: {
+        height: 50,
+      },
+    });
     return (
-      
-        <div>Hello would<div>
+      <MuiThemeProvider muiTheme={muiTheme}>
+          <div className = "bg">
+            ddddd
+          </div>
+      </MuiThemeProvider>
     );
   }
 }
